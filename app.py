49ba526e0ce1ce7d3269a337c29c44b51e9205c1
shivangi <mohapatralovu@gmail.com>
2026-07-29 01:31:55 +0530

    feat: add /analyze route skeleton

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/analyze', methods=['POST'])
 def home():
     return render_template('index.html')
+def analyze():
+    return "Route is working!"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
